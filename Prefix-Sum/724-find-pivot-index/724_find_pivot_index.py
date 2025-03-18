ad5b8ff0c0bb04fc9f27eab44583435988a4f202
viernesprin <viernesprincess0@gmail.com>
2025-03-17 22:34:25 -0400

    merge

--- Prefix-Sum/724-find-pivot-index/724_find_pivot_index.py
+++ Prefix-Sum/724-find-pivot-index/724_find_pivot_index.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution(object):
     def pivotIndex(self, nums):
         total_sum = sum(nums)
@@ -12,19 +11,4 @@ class Solution(object):
         
             left_sum += num
             
-=======
-class Solution(object):
-    def pivotIndex(self, nums):
-        total_sum = sum(nums)
-        left_sum = 0
-    
-        for i, num in enumerate(nums):
-            right_sum = total_sum - left_sum - num
-        
-            if left_sum == right_sum:
-                return i
-        
-            left_sum += num
-            
->>>>>>> 217cbfda8fc661c365c68536e588296e8150d6ea
         return -1
