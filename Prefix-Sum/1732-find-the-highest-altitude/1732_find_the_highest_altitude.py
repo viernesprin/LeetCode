eaa82780a0ceb7afd1c406ebb5ea65b1eb271bd5
viernesprin <viernesprincess0@gmail.com>
2025-03-17 22:35:25 -0400

    merge

--- Prefix-Sum/1732-find-the-highest-altitude/1732_find_the_highest_altitude.py
+++ Prefix-Sum/1732-find-the-highest-altitude/1732_find_the_highest_altitude.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution(object):
     def largestAltitude(self, gain):
         altitudes = [0]
@@ -10,17 +9,4 @@ class Solution(object):
 
             if temp > max:
                 max = temp
-=======
-class Solution(object):
-    def largestAltitude(self, gain):
-        altitudes = [0]
-        max = 0
-
-        for i in range(len(gain)):
-            temp = gain[i] + altitudes [i]
-            altitudes.append(temp)
-
-            if temp > max:
-                max = temp
->>>>>>> 217cbfda8fc661c365c68536e588296e8150d6ea
         return max
